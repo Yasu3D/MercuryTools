2c7d51603d7d53e7bd6e4ccfdc0d50cccd9b033b
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-03-17 22:32:11 +0100

    add some tooltips + change icons

--- Views/Tabs/PlateTableView.axaml.cs
+++ Views/Tabs/PlateTableView.axaml.cs
@@ -10,18 +10,18 @@ public partial class PlateTableView : UserControl
     {
         InitializeComponent();
 
-        Explorer.ButtonExport.Click += ButtonExport_OnClick;
-        Explorer.ButtonImport.Click += ButtonImport_OnClick;
+        Explorer.ButtonSave.Click += ButtonSave_OnClick;
+        Explorer.ButtonOpen.Click += ButtonOpen_OnClick;
         Explorer.ButtonAddElement.Click += ButtonAddElement_OnClick;
         Explorer.ButtonDeleteElement.Click += ButtonDeleteElement_OnClick;
     }
 
-    private void ButtonExport_OnClick(object? sender, RoutedEventArgs args)
+    private void ButtonSave_OnClick(object? sender, RoutedEventArgs args)
     {
         
     }
     
-    private void ButtonImport_OnClick(object? sender, RoutedEventArgs args)
+    private void ButtonOpen_OnClick(object? sender, RoutedEventArgs args)
     {
         
     }
