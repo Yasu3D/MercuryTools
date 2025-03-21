73827641cdb687b9247862c3ad8d8fba1ae0e55a
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-03-21 02:30:46 +0100

    monkaS

---
+++ Views/ArrayEditor.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MercuryTools.Views;
+
+public partial class ArrayEditor : UserControl
+{
+    public ArrayEditor()
+    {
+        InitializeComponent();
+    }
+}
